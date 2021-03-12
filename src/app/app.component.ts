a94de223c268a3f10b9ce48a2ccc880953e28ee2
oujeon <oujeon@gmail.com>
2021-03-12 20:57:20 +0900

    login 기능 추가
    login 기능 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 //
 import { AppService } from './app.service';
 //
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 //
 export class AppComponent implements OnInit {
@@ -16,7 +16,11 @@ export class AppComponent implements OnInit {
   major = 1;
   minor = 23;
   // constructor
-  constructor(private router: Router, private http: HttpClient, private _app: AppService) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private _app: AppService
+  ) {}
   // ngOnInit
   public ngOnInit() {
     let am = this;
@@ -26,38 +30,38 @@ export class AppComponent implements OnInit {
       }
     });
     // 로그인 체크
-    am.http.get<any>(`http://evci.duckdns.org:9090/v1/setting/my_account`, {
-      headers: new HttpHeaders({
-        "Content-Type": "application/json"
-      }),
-      withCredentials: true
-    })
-      .subscribe(res => {
-        // 
+    // am.http.get<any>(`http://evci.duckdns.org:9090/v1/setting/my_account`, {
+    //   headers: new HttpHeaders({
+    //     "Content-Type": "application/json"
+    //   }),
+    //   withCredentials: true
+    // })
+    //   .subscribe(res => {
+    //     //
 
-        console.log(res);
-        console.log(res);
-        console.log(res);
-        console.log(res);
-        // 
-        if (res.code === 0) {
-          this.router.navigateByUrl("/dashboard").then(e => {
-            if (e) {
-              console.log(e);
-            } else {
-              console.log(e);
-            }
-          });
-        } else {
-          this.router.navigateByUrl("/login").then(e => {
-            if (e) {
-              console.log(e);
-            } else {
-              console.log(e);
-            }
-          });
-        }
-      })
+    //     console.log(res);
+    //     console.log(res);
+    //     console.log(res);
+    //     console.log(res);
+    //     //
+    //     if (res.code === 0) {
+    //       this.router.navigateByUrl("/dashboard").then(e => {
+    //         if (e) {
+    //           console.log(e);
+    //         } else {
+    //           console.log(e);
+    //         }
+    //       });
+    //     } else {
+    //       this.router.navigateByUrl("/login").then(e => {
+    //         if (e) {
+    //           console.log(e);
+    //         } else {
+    //           console.log(e);
+    //         }
+    //       });
+    //     }
+    //   })
     //
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,111 +1,74 @@
 // import
-import { Component } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-import { Router } from "@angular/router";
-import { Login } from './login';
+import { Router } from '@angular/router';
 // Component
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 // LoginComponent
 export class LoginComponent {
-  // interface
-  login: Login = {
-    logo: '../../../assets/logo.jpg',
-    userId: '',
-    password: '',
-    authority: '',
-    company: '',
-    email: '',
-    passwordChangedDate: '',
-    userName: '',
-    code: '',
-    msg: ''
+  // state
+  mainLocalData: {
+    image: {
+      logo: '../../../assets/logo.jpg';
+    };
+    class: {
+      errorMessage1: false;
+      errorMessage2: false;
+      errorMessage3: false;
+    };
+    data: {
+      userId: null;
+      password: null;
+      rememberId: false;
+    };
   };
   // constructor
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    //
+    //
+  }
   // ngOnInit
   ngOnInit() {
-  }
-  //
-  async fnLogin(): Promise<any> {
-    let am = this;
-    // 
-    let URL = `http://evci.duckdns.org:9090/v1/login`;
-    //userId=neocyon0001&password=neo1234!
-    let body = new URLSearchParams();
-    body.set('userId', this.login.userId);
-    body.set('password', this.login.password);
     //
-    let header = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }),
-      withCredentials: true
-    }
-    let res1 = await am.http.post<any>(URL, body.toString(), header).toPromise();
-    // 
-    if (res1.code === 200) {
-      //
-      this.login.authority = res1.user.authorities[0].authority;
-      this.login.company = res1.user.company;
-      this.login.email = res1.user.email;
-      this.login.passwordChangedDate = res1.user.passwordChangedDate;
-      this.login.userName = res1.user.userName;
-      this.login.code = res1.code;
-      this.login.msg = res1.msg;
-      //
-      this.router.navigateByUrl("/dashboard").then(e => {
-
-      });
-    } else {
-      this.login.code = res1.code;
-      this.login.msg = res1.msg;
-    }
-  }
-  //
-  fnSample: {
-    // // 
-    // this.checkLogin()
-    //   .then((data) => {
-    //     am._code = data.code;
-    //     if (am._code !== 0) {
-    //       this.router.navigateByUrl("/login");
-    //     } else {
-    //       am.router.navigateByUrl("/dashboard");
-    //     }
-    //   })
-    //   .finally(function () {
-    //     // 
-    //   });
-    // // 
-    // this.checkLogin()
-    //   .then((data) => {
-    //     am._code = data.code;
-    //     if (this._code !== 0) {
-    //       this.router.navigateByUrl("/login");
-    //     } else {
-    //       this.router.navigateByUrl("/dashboard");
-    //     }
-    //   })
-    //   .finally(function () {
-    //   });
-    //     let am = this;
-    // let res1 = await am.http
-    //   .get<any>(`https://evci.duckdns.org/external/general/time?tz=Asia/Seoul`)
-    //   .toPromise();
-    // let res2 = await am.http
-    //   .get<any>(`https://evci.duckdns.org/v1/setting/my_account`)
-    //   .toPromise();
-    // return res2;
+    //
   }
   //
   setUserId(value): void {
+    //
+    //
+  }
+  //
+  getUserId(): string {
+    let me = this;
+    return '';
   }
   //
   setPassword(value): void {
+    //
+    //
+  }
+  //
+  getPassword(): string {
+    let me = this;
+    return '';
+  }
+  //
+  setRememberId(value): void {
+    //
+    //
+  }
+  //
+  getRememberId(): string {
+    let me = this;
+    return '';
+  }
+  //
+  doLoginFn(): void {
+    let me = this;
   }
 }
